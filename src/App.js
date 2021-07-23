f51ec1106b8bc37c63b7772176ef90a5b3c2b0aa
hariprasath14 <hariprasath14799@gmail.com>
2021-07-23 16:43:21 +0530

    test map()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var x=[1,2,3,4]
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +10,7 @@ function App() {
         <p>
           Hi From Hari
         </p>
+        {x.map((y)=><li>{y}</li>)}
         <p>my live site is now auto deploye on change</p>
       </header>
     </div>
